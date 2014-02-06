4e1c9e3ae4eb90be2d2f68d156fdab39e3e3b7be
vanas <jwc.vanas@gmail.com>
2014-02-07 00:58:41 +0100

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -20,7 +20,7 @@ g_settings = { \
     'port_pms'        :('32400',), \
     \
     'enable_dnsserver':('True', 'False'), \
-    'ip_dnsmaster'    :('8.8.8.8',), \
+    'ip_dnsmaster'    :('192.168.2.254',), \
     \
     
     'ip_webserver'    :('0.0.0.0',), \
